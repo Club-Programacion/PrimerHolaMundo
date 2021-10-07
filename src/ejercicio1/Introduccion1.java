a8c280cb02216494fedc8c53debb446babcbf34b
CxrlosMX <LuisCRendon131@gmail.com>
2021-10-07 09:34:33 -0500

    Primer hola mundo

---
+++ src/ejercicio1/Introduccion1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 07-oct-2021
+ *
+ */
+public class Introduccion1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola estoy aprendiendo programción");
+    }
+
+}
